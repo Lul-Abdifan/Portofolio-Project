65d326f8ffa06d6eb557c15b03bfd161aa1d447e
A <abdibekele94@gmail.com>
2022-10-11 05:09:11 -0400

    Add overlay links direction to its content

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const openBtn = document.getElementById('openBtn');
 const closeBtn = document.getElementById('closeBtn');
 const overlay = document.getElementById('overlay');
 
+
 openBtn.onclick = () => {
   overlay.style.display = 'block';
   document.body.style.position = 'fixed';
@@ -11,3 +12,9 @@ closeBtn.onclick = () => {
   overlay.style.display = 'none';
   document.body.style.position = 'relative';
 };
+function close() {
+   overlay.style.display = 'none'; 
+   document.body.style.position = 'relative'; 
+  } 
+  document.querySelectorAll('.overlay_link').forEach((btn) => { 
+    btn.onclick = () => { close(); }; }); 
