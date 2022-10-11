895f06aaa52aee08a371b65afa3b9cfc721570b1
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-10-11 14:38:00 +0630

    I remove some javascript code

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ const overlay = document.getElementById('overlay');
 
 openBtn.onclick = () => {
   overlay.style.display = 'block';
-  openBtn.style.display = 'none';
   document.body.style.position = 'fixed';
 };
 
 closeBtn.onclick = () => {
   overlay.style.display = 'none';
-  openBtn.style.display = 'block';
   document.body.style.position = 'relative';
 };
