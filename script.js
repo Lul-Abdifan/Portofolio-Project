936fffb7e85d8c7d7c34c7629bef3b524d9aad89
A <abdibekele94@gmail.com>
2022-10-20 16:02:05 -0400

    Add function for storing and loading in local storage

--- script.js
+++ script.js
@@ -25,30 +25,33 @@ document.querySelectorAll('.overlay_link').forEach((btn) => {
 const nameInput =document.getElementById('name');
 const emailInput =document.getElementById('email');
 const commentInput =document.getElementById('comment');
-const formsInput =document.getElementById('formSubmit');
 
 window.addEventListener('load',function(){
   getStoredData();
 })
-formsInput.forEach((input).addEventListener('change',function(){
- saveData()
+
+
+function getStoredData()
+{
+  const storedData =JSON.parse(localStorage.getItem("storedData"));
+  nameInput.value=storedData.name;
+  emailInput.value=storedData.email;
+  commentInput.value =storedData.comment;
 }
 
-))
 
-function saveData(){
-  var datas={
+document.getElementById('formSubmit').addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const datas={
     name:nameInput.value.trim(),
     email:emailInput.value.trim(),
     comment:commentInput.value.trim()
   };
   localStorage.setItem("storedData",JSON.stringify(datas));
-}
 
-function getStoredData()
-{
-  const storedData =JSON.parse(localStorage.getItem(storedData));
-  nameInput.value=storedData.name;
-  emailInput.value=storedeData.email;
-  commentInput.value =storedData.comment;
-}
+})
+
+
+
+
+
